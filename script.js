34696ad518d88700ad53ab9495a04540c449bd43
Sharmila <40612676+SharmilaSrinivasa@users.noreply.github.com>
2020-04-12 02:23:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,15 @@ function start()
 // Reset everything:
 function reset()
 {
-    console.log("Reset button activated");
+    //console.log("Reset button activated");
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
 }
 
 
